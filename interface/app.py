e8d7d1023a883fcd5f52b587abe6052c0dfb6161
EmelyChavez <00097923@uca.edu.sv>
2025-05-25 13:56:54 -0600

    ⚡️: Interface for both models

--- interface/app.py
+++ interface/app.py
@@ -3,32 +3,82 @@ import numpy as np
 import pandas as pd
 import joblib
 from logistic_model import OneVsRestLogisticRegression, LogisticRegressionGD
+from linear_model import LaptopPricePredictor
 
+# carga el modelo Parkinson
 _ = OneVsRestLogisticRegression
 _ = LogisticRegressionGD
-
-# carga los archivos kt que contienen el model, scale y feature previamente entrenado en el notebook
 modelo = joblib.load("interface/modelo_parkinson.pkl")
 scaler = joblib.load("interface/scaler_parkinson.pkl")
 features = joblib.load("interface/features_parkinson.pkl")
 
-#realiza la prediccion y se conecta a la interfaz
+# carga el modelo de laptop
+predictor = LaptopPricePredictor()
+predictor.fit()
+
+#funcion de predicion de parkinson
 def predecir_parkinson(*inputs):
     datos = np.array([inputs])
-    datos_df = pd.DataFrame(datos, columns=features)
-    datos_escalados = scaler.transform(datos_df)
+    df = pd.DataFrame(datos, columns=features)
+    datos_escalados = scaler.transform(df)
     pred = modelo.predict(datos_escalados)
-    return "Parkinson" if pred[0] == 1 else "Sano"
+    return "Persona con Parkinson" if pred[0] == 1 else "Persona Sana"
 
-#crea las entradas de tipo numerico para cada caracteristica
-inputs = [gr.Number(label=feat) for feat in features]
+# funcion de predicción Laptop
+def predecir_laptop(
+    Company, Product, TypeName, Inches, ScreenResolution, OpSys,
+    Weight, Ram, Cpu, Gpu, Memory
+):
+    input_data = {
+        "Company": Company,
+        "Product": Product,
+        "TypeName": TypeName,
+        "Inches": Inches,
+        "ScreenResolution": ScreenResolution,
+        "OpSys": OpSys,
+        "Weight": Weight,
+        "Ram": Ram,
+        "Cpu": Cpu,
+        "Gpu": Gpu,
+        "Memory": Memory
+    }
+    price = predictor.predict_price_usd(input_data)
+    return f"${price:,.2f} USD"
 
-#configuracion de la interfaz de gradio
-demo = gr.Interface(
+interface_parkinson = gr.Interface(
     fn=predecir_parkinson,
-    inputs=inputs,
-    outputs=gr.Text(label="Resultado"),
-    title="Clasificador de Parkinson"
+    inputs=[gr.Number(label=feat) for feat in features],
+    outputs=gr.Textbox(label="Resultado"),
+    title="Clasificador de Parkinson",
+    theme="soft"
+)
+
+interface_laptop = gr.Interface(
+    fn=predecir_laptop,
+    inputs=[
+        gr.Textbox(label="Company"),
+        gr.Textbox(label="Product"),
+        gr.Textbox(label="TypeName"),
+        gr.Number(label="Inches", precision=2),
+        gr.Textbox(label="ScreenResolution"),
+        gr.Textbox(label="OpSys"),
+        gr.Textbox(label="Weight"),
+        gr.Textbox(label="RAM"),
+        gr.Textbox(label="CPU"),
+        gr.Textbox(label="GPU"),
+        gr.Textbox(label="Memory"),
+    ],
+    outputs=gr.Textbox(label="Precio estimado (USD)"),
+    title="Predicción de Precio de Laptop",
+    theme="soft"
+)
+
+#tab con las dos interfaces
+
+demo = gr.TabbedInterface(
+    [interface_parkinson, interface_laptop],
+    tab_names=["Clasificador de Parkinson \n(Modelo de regresión logística)", "Predicción de Laptop \n(Modelo de regresión lineal)"],
+    theme="soft"
 )
 
 if __name__ == "__main__":
